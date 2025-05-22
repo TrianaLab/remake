12a53612d5414a7ea3b81f6dc241596dc429a57a
eduardodiaz-merlyn <easencio@merlyn.org>
2025-05-22 13:18:20 +0200

    fixed run

--- internal/run/run.go
+++ internal/run/run.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/TrianaLab/remake/config"
 )
@@ -21,8 +22,9 @@ func New(cfg *config.Config) Runner {
 }
 
 func (r *ExecRunner) Run(ctx context.Context, path string, targets []string) error {
-	cmd := exec.CommandContext(ctx, "make", targets...)
-	cmd.Dir = path
+	cmd := exec.CommandContext(ctx, "make", "-f", path)
+	cmd.Args = append(cmd.Args, targets...)
+	cmd.Dir = filepath.Dir(path)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
